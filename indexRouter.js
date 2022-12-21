f9659aa9071bae7b605f0a4105a7d9149d109b5f
be-thomas <thomas@hitwicket.com>
2022-12-21 17:43:04 +0530

    added attachments page

--- indexRouter.js
+++ indexRouter.js
@@ -4,7 +4,7 @@ let indexRouter = express.Router();
 
 indexRouter.use("/auth", require("./controllers/auth"))
 indexRouter.use("/tasks", require("./controllers/tasks"))
-
+indexRouter.use("/attachments", require("./controllers/attachments"))
 
 
 
